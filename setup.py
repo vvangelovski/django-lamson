4b02b03d06553b1b8661bd79ac6f8b8b1be84c1f
Vasil Vangelovski <vvangelovski@gmail.com>
2012-01-20 12:02:26 +0100

    Project structure

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os, sys
+from setuptools import setup, find_packages
+
+import django_lamson
+from django_lamson import VERSION, __version__
+
+if VERSION[-1] == 'final':
+    STATUS = ['Development Status :: 5 - Production/Stable']
+elif 'beta' in VERSION[-1]:
+    STATUS = ['Development Status :: 4 - Beta']
+else:
+    STATUS = ['Development Status :: 3 - Alpha']
+
+def get_readme():
+    try:
+        return  open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+    except IOError:
+        return ''
+
+setup(
+    name = 'django-lamson',
+    version = __version__,
+    packages = find_packages(exclude = ['testproject']),
+    author = 'Vasil Vangelovski',
+    author_email = 'vvangelovski@gmail.com',
+    license = 'New BSD License (http://www.opensource.org/licenses/bsd-license.php)',
+    description = 'Easy integration of lamson in django projects',
+    long_description = get_readme(),
+    url = 'https://github.com/vvangelovski/django-lamson',
+    download_url = 'https://github.com/vvangelovski/django-lamson',
+    include_package_data = True,
+    zip_safe = False,
+    
+    classifiers = STATUS + [
+       'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
